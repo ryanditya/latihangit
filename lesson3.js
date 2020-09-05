94d9a845deb4775947ab151dfa5190bff00215eb
Ryan Aditya <ryanwahyudhi17@gmail.com>
2020-09-05 23:15:46 +0800

    add lesson3.js

---
+++ lesson3.js
@@ -0,0 +1,21 @@
+//anonymous function / arrow function
+//function biasa
+
+function PrintHello() {
+    console.log("Hello Semua");
+}
+PrintHello();
+
+//arrow function
+
+const PrintHello2 = () => {
+    console.log("Hello Semua Lagi");
+}
+
+PrintHello2();
+
+const ShowTeks = (teks) => {
+    console.log(teks);
+}
+
+ShowTeks("nama saya Ryan");
