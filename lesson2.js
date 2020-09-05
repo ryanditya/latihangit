90bdccab6619983fdb7aacee880682c8e79a5dad
Ryan Aditya <ryanwahyudhi17@gmail.com>
2020-09-05 21:42:01 +0800

    commit kedua

---
+++ lesson2.js
@@ -0,0 +1,15 @@
+// for (var i = 0; i <= 3; i++) {
+
+//     if (i == 2) {
+//         function show() {
+//             console.log(i)
+//         }
+//     }
+
+// }
+
+// show();
+
+
+const nama = 'ryan';
+console.log(nama);
